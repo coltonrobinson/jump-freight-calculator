f41bf3a28702b4384669f1c2b8fe78c0c57f0027
Colton <colton@bluemel.me>
2024-01-29 09:54:13 -0700

    Added title and changed favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import styles from "./page.module.css";
-import Calculator from "../../calculator";
+import Calculator from "./calculator";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.app}>
-      <div className={styles.scroll_card}>
-        <Calculator />
+    <>
+      <Head>
+        <link rel="icon" href="./images/favicon-32x32.png" sizes="any" />
+      </Head>
+      <div className={styles.app}>
+        <div className={styles.scroll_card}>
+          <Calculator />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
